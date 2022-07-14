c7493f7ff2e31c74a7a6c2168cc4502af793cb63
MarcoCiaramella <marcociaramella87@gmail.com>
2022-07-14 15:30:36 +0200

    removed try catch

--- index.js
+++ index.js
@@ -178,142 +178,137 @@ let device;
  */
 export async function sha256(bytesArray) {
 
-    try {
-        device = device ? device : await getGPUDevice();
+    device = device ? device : await getGPUDevice();
 
-        const numWorkgroups = calcNumWorkgroups(device, bytesArray);
+    const numWorkgroups = calcNumWorkgroups(device, bytesArray);
 
-        const messages = [];
-        let bufferSize = 0;
-        const messageSizes = getMessageSizes(bytesArray[0]);
-        bytesArray.forEach(bytes => {
-            if (bytes.length % 4 !== 0) throw "Message must be 32-bit aligned";
-            const message = padMessage(bytes, messageSizes[1]);
-            // message is the padded version of the input message as dscribed by SHA-256 specification
-            messages.push(message);
-            // messages has same size
-            bufferSize += message.byteLength;
-        });
-        const numMessages = messages.length;
+    const messages = [];
+    let bufferSize = 0;
+    const messageSizes = getMessageSizes(bytesArray[0]);
+    for (const bytes of bytesArray) {
+        if (bytes.length % 4 !== 0) throw "Message must be 32-bit aligned";
+        const message = padMessage(bytes, messageSizes[1]);
+        // message is the padded version of the input message as dscribed by SHA-256 specification
+        messages.push(message);
+        // messages has same size
+        bufferSize += message.byteLength;
+    }
+    const numMessages = messages.length;
 
-        // build shader input data
-        const messageArray = new Uint32Array(new ArrayBuffer(bufferSize));
-        let offset = 0;
-        messages.forEach(message => {
-            messageArray.set(message, offset);
-            offset += message.length;
-        });
+    // build shader input data
+    const messageArray = new Uint32Array(new ArrayBuffer(bufferSize));
+    let offset = 0;
+    for (const message of messages) {
+        messageArray.set(message, offset);
+        offset += message.length;
+    }
 
-        // messages
-        const messageArrayBuffer = device.createBuffer({
-            mappedAtCreation: true,
-            size: messageArray.byteLength,
-            usage: GPUBufferUsage.STORAGE
-        });
-        new Uint32Array(messageArrayBuffer.getMappedRange()).set(messageArray);
-        messageArrayBuffer.unmap();
+    // messages
+    const messageArrayBuffer = device.createBuffer({
+        mappedAtCreation: true,
+        size: messageArray.byteLength,
+        usage: GPUBufferUsage.STORAGE
+    });
+    new Uint32Array(messageArrayBuffer.getMappedRange()).set(messageArray);
+    messageArrayBuffer.unmap();
 
-        // num_messages
-        const numMessagesBuffer = device.createBuffer({
-            mappedAtCreation: true,
-            size: Uint32Array.BYTES_PER_ELEMENT,
-            usage: GPUBufferUsage.STORAGE
-        });
-        new Uint32Array(numMessagesBuffer.getMappedRange()).set([bytesArray.length]);
-        numMessagesBuffer.unmap();
+    // num_messages
+    const numMessagesBuffer = device.createBuffer({
+        mappedAtCreation: true,
+        size: Uint32Array.BYTES_PER_ELEMENT,
+        usage: GPUBufferUsage.STORAGE
+    });
+    new Uint32Array(numMessagesBuffer.getMappedRange()).set([bytesArray.length]);
+    numMessagesBuffer.unmap();
 
-        // message_sizes
-        const messageSizesBuffer = device.createBuffer({
-            mappedAtCreation: true,
-            size: messageSizes.byteLength,
-            usage: GPUBufferUsage.STORAGE
-        });
-        new Uint32Array(messageSizesBuffer.getMappedRange()).set(messageSizes);
-        messageSizesBuffer.unmap();
+    // message_sizes
+    const messageSizesBuffer = device.createBuffer({
+        mappedAtCreation: true,
+        size: messageSizes.byteLength,
+        usage: GPUBufferUsage.STORAGE
+    });
+    new Uint32Array(messageSizesBuffer.getMappedRange()).set(messageSizes);
+    messageSizesBuffer.unmap();
 
-        // Result
-        const resultBufferSize = (256 / 8) * numMessages;
-        const resultBuffer = device.createBuffer({
-            size: resultBufferSize,
-            usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
-        });
+    // Result
+    const resultBufferSize = (256 / 8) * numMessages;
+    const resultBuffer = device.createBuffer({
+        size: resultBufferSize,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+    });
 
-        const shaderModule = device.createShaderModule({
-            code: shader
-        });
+    const shaderModule = device.createShaderModule({
+        code: shader
+    });
 
-        const computePipeline = device.createComputePipeline({
-            compute: {
-                module: shaderModule,
-                entryPoint: "sha256"
-            },
-            layout: 'auto'
-        });
+    const computePipeline = device.createComputePipeline({
+        compute: {
+            module: shaderModule,
+            entryPoint: "sha256"
+        },
+        layout: 'auto'
+    });
 
-        const bindGroup = device.createBindGroup({
-            layout: computePipeline.getBindGroupLayout(0),
-            entries: [
-                {
-                    binding: 0,
-                    resource: {
-                        buffer: messageArrayBuffer
-                    }
-                },
-                {
-                    binding: 1,
-                    resource: {
-                        buffer: numMessagesBuffer
-                    }
-                },
-                {
-                    binding: 2,
-                    resource: {
-                        buffer: messageSizesBuffer
-                    }
-                },
-                {
-                    binding: 3,
-                    resource: {
-                        buffer: resultBuffer
-                    }
+    const bindGroup = device.createBindGroup({
+        layout: computePipeline.getBindGroupLayout(0),
+        entries: [
+            {
+                binding: 0,
+                resource: {
+                    buffer: messageArrayBuffer
                 }
-            ]
-        });
+            },
+            {
+                binding: 1,
+                resource: {
+                    buffer: numMessagesBuffer
+                }
+            },
+            {
+                binding: 2,
+                resource: {
+                    buffer: messageSizesBuffer
+                }
+            },
+            {
+                binding: 3,
+                resource: {
+                    buffer: resultBuffer
+                }
+            }
+        ]
+    });
 
-        const commandEncoder = device.createCommandEncoder();
+    const commandEncoder = device.createCommandEncoder();
 
-        const passEncoder = commandEncoder.beginComputePass();
-        passEncoder.setPipeline(computePipeline);
-        passEncoder.setBindGroup(0, bindGroup);
-        passEncoder.dispatchWorkgroups(numWorkgroups);
-        passEncoder.end();
+    const passEncoder = commandEncoder.beginComputePass();
+    passEncoder.setPipeline(computePipeline);
+    passEncoder.setBindGroup(0, bindGroup);
+    passEncoder.dispatchWorkgroups(numWorkgroups);
+    passEncoder.end();
 
-        const gpuReadBuffer = device.createBuffer({
-            size: resultBufferSize,
-            usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
-        });
-        commandEncoder.copyBufferToBuffer(
-            resultBuffer,
-            0,
-            gpuReadBuffer,
-            0,
-            resultBufferSize
-        );
+    const gpuReadBuffer = device.createBuffer({
+        size: resultBufferSize,
+        usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+    });
+    commandEncoder.copyBufferToBuffer(
+        resultBuffer,
+        0,
+        gpuReadBuffer,
+        0,
+        resultBufferSize
+    );
 
-        const gpuCommands = commandEncoder.finish();
-        device.queue.submit([gpuCommands]);
+    const gpuCommands = commandEncoder.finish();
+    device.queue.submit([gpuCommands]);
 
-        await gpuReadBuffer.mapAsync(GPUMapMode.READ);
+    await gpuReadBuffer.mapAsync(GPUMapMode.READ);
 
-        const hashSize = 256 / 8;
-        const hashes = [];
-        for (let i = 0; i < numMessages; i++) {
-            hashes.push(new Uint8Array(gpuReadBuffer.getMappedRange(i * hashSize, hashSize)));
-        }
-
-        return hashes;
+    const hashSize = 256 / 8;
+    const hashes = [];
+    for (let i = 0; i < numMessages; i++) {
+        hashes.push(new Uint8Array(gpuReadBuffer.getMappedRange(i * hashSize, hashSize)));
     }
-    catch (error) {
-        console.error(error);
-    }
+
+    return hashes;
 }
