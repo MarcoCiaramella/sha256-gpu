cc9ff0c921a371d2c30fe441cf3ee83e768fbd3e
MarcoCiaramella <marcociaramella87@gmail.com>
2022-07-19 01:35:42 +0200

    refactoring

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ fn ch(e: u32, f: u32, g: u32) -> u32 {
 @group(0) @binding(0) var<storage, read_write> messages: array<u32>;
 @group(0) @binding(1) var<storage, read> num_messages: u32;
 @group(0) @binding(2) var<storage, read> message_sizes: array<u32>;
-@group(0) @binding(3) var<storage, read_write> hash: array<u32>;
+@group(0) @binding(3) var<storage, read_write> hashes: array<u32>;
 
 @compute @workgroup_size(${device.limits.maxComputeWorkgroupSizeX})
 fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
@@ -64,14 +64,14 @@ fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
 
     // == processing == //
 
-    hash[hash_base_index] = 0x6a09e667u;
-    hash[hash_base_index + 1] = 0xbb67ae85u;
-    hash[hash_base_index + 2] = 0x3c6ef372u;
-    hash[hash_base_index + 3] = 0xa54ff53au;
-    hash[hash_base_index + 4] = 0x510e527fu;
-    hash[hash_base_index + 5] = 0x9b05688cu;
-    hash[hash_base_index + 6] = 0x1f83d9abu;
-    hash[hash_base_index + 7] = 0x5be0cd19u;
+    hashes[hash_base_index] = 0x6a09e667u;
+    hashes[hash_base_index + 1] = 0xbb67ae85u;
+    hashes[hash_base_index + 2] = 0x3c6ef372u;
+    hashes[hash_base_index + 3] = 0xa54ff53au;
+    hashes[hash_base_index + 4] = 0x510e527fu;
+    hashes[hash_base_index + 5] = 0x9b05688cu;
+    hashes[hash_base_index + 6] = 0x1f83d9abu;
+    hashes[hash_base_index + 7] = 0x5be0cd19u;
 
     let k = array<u32,64>(
         0x428a2f98u, 0x71374491u, 0xb5c0fbcfu, 0xe9b5dba5u, 0x3956c25bu, 0x59f111f1u, 0x923f82a4u, 0xab1c5ed5u,
@@ -94,14 +94,14 @@ fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
         for (var j = 16u; j < 64u; j++){
             w[j] = w[j - 16u] + g0(w[j - 15u]) + w[j - 7u] + g1(w[j - 2u]);
         }
-        var a = hash[hash_base_index];
-        var b = hash[hash_base_index + 1];
-        var c = hash[hash_base_index + 2];
-        var d = hash[hash_base_index + 3];
-        var e = hash[hash_base_index + 4];
-        var f = hash[hash_base_index + 5];
-        var g = hash[hash_base_index + 6];
-        var h = hash[hash_base_index + 7];
+        var a = hashes[hash_base_index];
+        var b = hashes[hash_base_index + 1];
+        var c = hashes[hash_base_index + 2];
+        var d = hashes[hash_base_index + 3];
+        var e = hashes[hash_base_index + 4];
+        var f = hashes[hash_base_index + 5];
+        var g = hashes[hash_base_index + 6];
+        var h = hashes[hash_base_index + 7];
         for (var j = 0u; j < 64u; j++){
             let t2 = s0(a) + maj(a, b, c);
             let t1 = h + s1(e) + ch(e, f, g) + k[j] + w[j];
@@ -114,23 +114,23 @@ fn sha256(@builtin(global_invocation_id) global_id: vec3<u32>) {
             b = a;
             a = t1 + t2;
         }
-        hash[hash_base_index] += a;
-        hash[hash_base_index + 1] += b;
-        hash[hash_base_index + 2] += c;
-        hash[hash_base_index + 3] += d;
-        hash[hash_base_index + 4] += e;
-        hash[hash_base_index + 5] += f;
-        hash[hash_base_index + 6] += g;
-        hash[hash_base_index + 7] += h;
+        hashes[hash_base_index] += a;
+        hashes[hash_base_index + 1] += b;
+        hashes[hash_base_index + 2] += c;
+        hashes[hash_base_index + 3] += d;
+        hashes[hash_base_index + 4] += e;
+        hashes[hash_base_index + 5] += f;
+        hashes[hash_base_index + 6] += g;
+        hashes[hash_base_index + 7] += h;
     }
-    hash[hash_base_index] = swap_endianess32(hash[hash_base_index]);
-    hash[hash_base_index + 1] = swap_endianess32(hash[hash_base_index + 1]);
-    hash[hash_base_index + 2] = swap_endianess32(hash[hash_base_index + 2]);
-    hash[hash_base_index + 3] = swap_endianess32(hash[hash_base_index + 3]);
-    hash[hash_base_index + 4] = swap_endianess32(hash[hash_base_index + 4]);
-    hash[hash_base_index + 5] = swap_endianess32(hash[hash_base_index + 5]);
-    hash[hash_base_index + 6] = swap_endianess32(hash[hash_base_index + 6]);
-    hash[hash_base_index + 7] = swap_endianess32(hash[hash_base_index + 7]);
+    hashes[hash_base_index] = swap_endianess32(hashes[hash_base_index]);
+    hashes[hash_base_index + 1] = swap_endianess32(hashes[hash_base_index + 1]);
+    hashes[hash_base_index + 2] = swap_endianess32(hashes[hash_base_index + 2]);
+    hashes[hash_base_index + 3] = swap_endianess32(hashes[hash_base_index + 3]);
+    hashes[hash_base_index + 4] = swap_endianess32(hashes[hash_base_index + 4]);
+    hashes[hash_base_index + 5] = swap_endianess32(hashes[hash_base_index + 5]);
+    hashes[hash_base_index + 6] = swap_endianess32(hashes[hash_base_index + 6]);
+    hashes[hash_base_index + 7] = swap_endianess32(hashes[hash_base_index + 7]);
 }`;
 }
 
